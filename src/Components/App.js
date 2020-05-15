b8537fc0bd439f62bbe05bcb1bc668861064d901
yogeshu <yogeshbhavsar1994@gmail.com>
2020-05-15 12:08:43 +0530

    The Folder structure - css and event handler added for UI

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+import SearchBar from "./SearchBar";
 class App extends Component {
-    state = {  }
-    render() { 
-        return ( 
+  state = {};
 
-            <div>
-                App 
-            </div>
-         );
-    }
+  
+   onSearchSubmit = term => {
+       console.log(term)
+   }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <SearchBar    onSubmit={ this.onSearchSubmit}  />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
