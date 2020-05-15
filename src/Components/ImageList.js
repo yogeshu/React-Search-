f8a9d507a89832a71bd57663becfc500deb449c3
yogeshu <yogeshbhavsar1994@gmail.com>
2020-05-15 13:47:07 +0530

    The Folder structure - added information of image  - UI

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const ImageList = props => {
     
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular}  />
+        return <img alt={image.description} key={image.id}  src={image.urls.regular}  />
     })
     return (
         <div>
