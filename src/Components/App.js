8e889c5e88e721314f8372e8e107702b6a4bd821
yogeshu <yogeshbhavsar1994@gmail.com>
2020-05-15 11:51:33 +0530

    The Folder structure

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+            <div>
+                App 
+            </div>
+         );
+    }
+}
+ 
+export default App;
